6236cf188ebbc7ceeaffbd0f4aba40c1539a1899
Sebald <sebastian.sebald@gmail.com>
2011-10-19 21:33:41 +0200

    added error indication to register form

--- application/views/account/register_form.php
+++ application/views/account/register_form.php
@@ -37,45 +37,50 @@
           );          
 ?>
 
-<?php echo form_open('account/create', $attr_form); ?>
+<?php echo form_open('account/register', $attr_form); ?>
     <div class="clearfix">
       <label>Username</label>
         <div class="input">
           <input id="username" type="text" disabled="" size="30" name="disabledInput" id="disabledInput" class="large disabled">
+          <span class="help-inline">This is automatically generated for you.</span>
         </div>
     </div>
-    <div class="clearfix">
+    <div class="clearfix <?php if(form_error('first_name')) echo 'error'; ?>">
       <?php echo form_label('First name:', 'first_name'); ?>
       <div class="input">
           <?php echo form_input($attr_input_first_name, set_value('first_name')); ?>
+          <?php echo form_error('first_name'); ?>
       </div>
     </div>
-    <div class="clearfix">
+    <div class="clearfix <?php if(form_error('last_name')) echo 'error'; ?>">
       <?php echo form_label('Last name:', 'last_name'); ?>
       <div class="input">
           <?php echo form_input($attr_input_last_name, set_value('last_name')); ?>
+          <?php echo form_error('last_name'); ?>
       </div>
     </div>    
-    <div class="clearfix">
+    <div class="clearfix <?php if(form_error('email')) echo 'error'; ?>">
       <?php echo form_label('Email:', 'email'); ?>
       <div class="input">
           <?php echo form_input($attr_input_email, set_value('email')); ?>
+          <?php echo form_error('email'); ?>
       </div>
     </div>
-    <div class="clearfix">
+    <div class="clearfix <?php if(form_error('password')) echo 'error'; ?>">
       <?php echo form_label('Password:', 'password'); ?>
       <div class="input">
           <?php echo form_password($attr_input_pwd); ?>
+          <?php echo form_error('password'); ?>
       </div>
     </div>
-    <div class="clearfix">
+    <div class="clearfix <?php if(form_error('password2')) echo 'error'; ?>">
       <?php echo form_label('Password confirm:', 'password2'); ?>
       <div class="input">
           <?php echo form_password($attr_input_pwd2); ?>
+          <?php echo form_error('password2'); ?>
       </div>
     </div> 
     <div class="actions">
       <?php echo form_submit('register', 'Register', 'class="btn large primary"'); ?>
     </div>
 <?php echo form_close(); ?>
-<?php echo validation_errors(); ?>
