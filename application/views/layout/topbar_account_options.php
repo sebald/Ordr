f4557324a5aa86dedc310ae15190475cab9d4f5b
Sebald <sebastian.sebald@gmail.com>
2011-10-23 16:12:09 +0200

    added settings ling in topbar

--- application/views/layout/topbar_account_options.php
+++ application/views/layout/topbar_account_options.php
@@ -2,7 +2,7 @@
   <li class="dropdown">
     <a class="dropdown-toggle" href="#">Account</a>
     <ul class="dropdown-menu">
-      <li><a href="#">Settings</a></li>
+      <li><?php echo anchor('account/settings', 'Settings'); ?></li>
       <li><a href="#">Help</a></li>
       <li class="divider"></li>
       <li><?php echo anchor('account/logout', 'Logout'); ?></li>
