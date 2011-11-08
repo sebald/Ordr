4513b441c940ac099f45d9fe6961fcbc337b531e
Sebald <sebastian.sebald@gmail.com>
2011-11-08 10:21:03 +0100

    layout register_form.php

--- application/views/account/register_form.php
+++ application/views/account/register_form.php
@@ -35,59 +35,64 @@
             'size'        => '30'
           );       
 ?>
-<?php if ( isset($user_already_exists) ) : ?>
-<div class="alert-message block-message error">
-  <p><strong>Oh snap! The user you want to create already exists.</strong> Try the following:</p>
-  <ul>
-    <li>Contact an admin from your office. Maybe (s)he already created a user for you.</li>
-    <li>Visit the help page.</li>
-  </ul>
-</div>
-<?php endif; ?>
-<?php echo form_open('account/register', $attr_form); ?>
-    <div class="clearfix">
-      <label>Username</label>
-        <div class="input">
-          <input id="username" type="text" disabled="" size="30" name="disabledInput" id="disabledInput" class="span4 disabled">
-          <span class="help-inline">This is automatically generated for you.</span>
+<div class="row">
+  <div class="span12">
+    <?php if ( isset($user_already_exists) ) : ?>
+    <div class="alert-message block-message error">
+      <p><strong>Sorry! The user you want to create already exists.</strong> Try the following:</p>
+      <ul>
+        <li>Contact an admin from your office. Maybe (s)he already created a user for you.</li>
+        <li>Visit the help page.</li>
+      </ul>
+    </div>
+    <?php endif; ?>
+    <?php echo form_open('account/register', $attr_form); ?>
+        <div class="clearfix">
+          <label>Username</label>
+            <div class="input">
+              <input id="username" type="text" disabled="" size="30" name="disabledInput" id="disabledInput" class="span4 disabled">
+              <span class="help-inline">This is automatically generated for you.</span>
+            </div>
         </div>
-    </div>
-    <div class="clearfix <?php if(form_error('first_name')) echo 'error'; ?>">
-      <?php echo form_label('First name:', 'first_name'); ?>
-      <div class="input">
-          <?php echo form_input($attr_input_first_name, set_value('first_name')); ?>
-          <?php echo form_error('first_name','<span class="help-inline">','</span>'); ?>
-      </div>
-    </div>
-    <div class="clearfix <?php if(form_error('last_name')) echo 'error'; ?>">
-      <?php echo form_label('Last name:', 'last_name'); ?>
-      <div class="input">
-          <?php echo form_input($attr_input_last_name, set_value('last_name')); ?>
-          <?php echo form_error('last_name','<span class="help-inline">','</span>'); ?>
-      </div>
-    </div>    
-    <div class="clearfix <?php if(form_error('email')) echo 'error'; ?>">
-      <?php echo form_label('Email:', 'email'); ?>
-      <div class="input">
-          <?php echo form_input($attr_input_email, set_value('email')); ?>
-          <?php echo form_error('email','<span class="help-inline">','</span>'); ?>
-      </div>
-    </div>
-    <div class="clearfix <?php if(form_error('password')) echo 'error'; ?>">
-      <?php echo form_label('Password:', 'password'); ?>
-      <div class="input">
-          <?php echo form_password($attr_input_pwd); ?>
-          <?php echo form_error('password','<span class="help-inline">','</span>'); ?>
-      </div>
-    </div>
-    <div class="clearfix <?php if(form_error('password2')) echo 'error'; ?>">
-      <?php echo form_label('Password confirm:', 'password2'); ?>
-      <div class="input">
-          <?php echo form_password($attr_input_pwd2); ?>
-          <?php echo form_error('password2','<span class="help-inline">','</span>'); ?>
-      </div>
-    </div> 
-    <div class="actions">
-      <?php echo form_submit('register', 'Register', 'class="btn span4 primary"'); ?>
-    </div>
-<?php echo form_close(); ?>
+        <div class="clearfix <?php if(form_error('first_name')) echo 'error'; ?>">
+          <?php echo form_label('First name:', 'first_name'); ?>
+          <div class="input">
+              <?php echo form_input($attr_input_first_name, set_value('first_name')); ?>
+              <?php echo form_error('first_name','<span class="help-inline">','</span>'); ?>
+          </div>
+        </div>
+        <div class="clearfix <?php if(form_error('last_name')) echo 'error'; ?>">
+          <?php echo form_label('Last name:', 'last_name'); ?>
+          <div class="input">
+              <?php echo form_input($attr_input_last_name, set_value('last_name')); ?>
+              <?php echo form_error('last_name','<span class="help-inline">','</span>'); ?>
+          </div>
+        </div>    
+        <div class="clearfix <?php if(form_error('email')) echo 'error'; ?>">
+          <?php echo form_label('Email:', 'email'); ?>
+          <div class="input">
+              <?php echo form_input($attr_input_email, set_value('email')); ?>
+              <?php echo form_error('email','<span class="help-inline">','</span>'); ?>
+          </div>
+        </div>
+        <div class="clearfix <?php if(form_error('password')) echo 'error'; ?>">
+          <?php echo form_label('Password:', 'password'); ?>
+          <div class="input">
+              <?php echo form_password($attr_input_pwd); ?>
+              <?php echo form_error('password','<span class="help-inline">','</span>'); ?>
+          </div>
+        </div>
+        <div class="clearfix <?php if(form_error('password2')) echo 'error'; ?>">
+          <?php echo form_label('Password confirm:', 'password2'); ?>
+          <div class="input">
+              <?php echo form_password($attr_input_pwd2); ?>
+              <?php echo form_error('password2','<span class="help-inline">','</span>'); ?>
+          </div>
+        </div> 
+        <div class="actions">
+          <?php echo form_submit('register', 'Register', 'class="btn span4 primary large"'); ?>
+          <?php echo anchor('/', 'Cancel', 'class="btn large"'); ?>
+        </div>
+    <?php echo form_close(); ?>
+  </div>
+</div> 
