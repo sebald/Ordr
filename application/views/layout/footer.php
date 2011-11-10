398bdf2b068cd40868140b6a49f4266401857190
Sebald <sebastian.sebald@gmail.com>
2011-11-10 13:27:34 +0100

    js-dropdown now local

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -4,7 +4,7 @@
   </footer>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script src="http://twitter.github.com/bootstrap/1.3.0/bootstrap-dropdown.js"></script>
+<script src="<?php echo base_url();?>js/bootstrap-dropdown.js"></script>
 <script src="<?php echo base_url();?>js/functions.js"></script>
 
 <body>
