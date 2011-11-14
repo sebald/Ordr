92a25ea8d0a70ccb7cd484d9d5f7ecdeb1457a4a
Sebald <sebastian.sebald@gmail.com>
2011-11-14 12:41:41 +0100

    added alerts.js (so jo can close the message)

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -6,6 +6,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
 <script src="<?php echo base_url();?>js/bootstrap-dropdown.js"></script>
 <script src="<?php echo base_url();?>js/bootstrap-modal.js"></script>
+<script src="<?php echo base_url();?>js/bootstrap-alerts.js"></script>
 <script src="<?php echo base_url();?>js/functions.js"></script>
 
 <body>
