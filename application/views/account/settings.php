ea469dc4ad0217700ad0daef4561ef70fcf3f0b2
Sebald <sebastian.sebald@gmail.com>
2012-01-09 01:04:25 +0100

    css+html for account/settings adjust it to bootstrap 2.0

--- application/views/account/settings.php
+++ application/views/account/settings.php
@@ -1,6 +1,5 @@
 <h1 class="page-header">Account Settings <small>Update your information.</small></h1>
-<div class="row">
-  <div class="span11">
+<div class="fluid-container">
   <?php if ( isset($error) ) : ?>
   <div class="alert-message block-message error">
     <p><strong>Oh snap! There was an problem with displaying your settings.</strong> Please try again.</p>
@@ -13,12 +12,12 @@
   <?php endif; ?>
   <?php
       $attr_form = array(
-              'class'       => 'settings'
+              'class'       => 'settings horizontal-form'
             );
       $attr_input_email = array(
               'name'        => 'email',
               'id'          => 'email',
-              'class'       => 'span4',
+              'class'       => 'span3',
               'size'        => '30'
             );       
       $attr_input_pwd = array(
@@ -41,75 +40,80 @@
             );            
   ?>
   <?php echo form_open('account/settings', $attr_form); ?>
-  <fieldset>
-    <legend>General</legend>
-    <div class="clearfix">
-      <label>Username</label>
-        <div class="input">
-          <input id="username" type="text" disabled="" size="30" name="username" id="disabledInput" class="span4 disabled" value="<?php echo isset($settings->username) ? $settings->username : ''; ?>">
-        </div>
-    </div>
-    <div class="clearfix">
-      <label>First Name</label>
-        <div class="input">
-          <input id="username" type="text" disabled="" size="30" name="first_name" id="disabledInput" class="span4 disabled" value="<?php echo isset($settings->first_name) ? $settings->first_name : ''; ?>">
-        </div>
-    </div>
-    <div class="clearfix">
-      <label>Last Name</label>
-        <div class="input">
-          <input id="username" type="text" disabled="" size="30" name="last_name" id="disabledInput" class="span4 disabled" value="<?php echo isset($settings->last_name) ? $settings->last_name : ''; ?>">
-        </div>
-    </div>  
-    <div class="clearfix <?php if(form_error('email')) echo 'error'; ?>">
-      <?php echo form_label('Email:', 'email'); ?>
-      <div class="input">
-          <?php echo form_input($attr_input_email, set_value('email', isset($settings->email) ? $settings->email : '')); ?>
-          <?php echo form_error('email','<span class="help-inline">','</span>'); ?>
-      </div>
-    </div>
-    <div class="clearfix">
-      <label>Role</label>
-        <div class="input">
-          <input id="role" type="text" disabled="" size="30" name="role" id="disabledInput" class="span3 disabled" value="<?php echo isset($settings->role) ? $settings->role : ''; ?>">
-        </div>
-    </div>
-  </fieldset>
-  <fieldset>
-    <legend>Change Password</legend>
-    <div class="clearfix <?php if(form_error('newpassword')) echo 'error'; ?>">
-    <?php echo form_label('New Password:', 'newpassword'); ?>
-    <div class="input">
-        <?php echo form_password($attr_input_pwd); ?>
-        <?php echo form_error('newpassword','<span class="help-inline">','</span>'); ?>
-    </div>
-    </div>
-    <div class="clearfix <?php if(form_error('newpassword2')) echo 'error'; ?>">
-      <?php echo form_label('Password confirm:', 'newpassword2'); ?>
-      <div class="input">
-          <?php echo form_password($attr_input_pwd2); ?>
-          <?php echo form_error('newpassword2','<span class="help-inline">','</span>'); ?>
-      </div>
-    </div>
-  </fieldset>
-  <fieldset> 
-    <div class="clearfix <?php if( isset($confirmation_error) ) echo 'error'; ?>">
-      <?php echo form_label('Confirmation:', 'confirmation'); ?>
-      <div class="input">
-          <?php echo form_password($attr_input_conf); ?>
-          <span class="help-inline">Enter your old password to confirm changes.</span>
-      </div>
-    </div>
-  <fieldset>  
-  <div class="actions">
-    <input type="submit" value="Save changes" class="btn primary">&nbsp;<button class="btn" type="reset">Cancel</button>
-  </div>    
-  <?php echo form_close(); ?>
+  <div class="row">
+  	<div class="span6">
+	  <legend>General</legend>
+	  <fieldset class="control-group">
+	          <label for="disabledInput" class="control-label">Username</label>
+	          <div class="controls">
+	            <input type="text" disabled="" placeholder="<?php echo isset($settings->username) ? $settings->username : ''; ?>" name="username" class="span3 disabled">
+	          </div>
+	  </fieldset>
+	  <fieldset class="control-group">
+	          <label for="disabledInput" class="control-label">First Name</label>
+	          <div class="controls">
+	            <input type="text" disabled="" placeholder="<?php echo isset($settings->username) ? $settings->first_name : ''; ?>" name="first_name" class="span3 disabled">
+	          </div>
+	  </fieldset>
+	  <fieldset class="control-group">
+	          <label for="disabledInput" class="control-label">Lase Name</label>
+	          <div class="controls">
+	            <input type="text" disabled="" placeholder="<?php echo isset($settings->username) ? $settings->last_name : ''; ?>" name="last_name" class="span3 disabled">
+	          </div>
+	  </fieldset>
+	  <fieldset class="control-group<?php if(form_error('email')) echo ' error'; ?>">
+	          <label for="email" class="control-label">Email</label>
+	          <div class="controls">
+		          <?php echo form_input($attr_input_email, set_value('email', isset($settings->email) ? $settings->email : '')); ?>
+		          <?php echo form_error('email','<p class="help-block">','</p>'); ?>
+	          </div>
+	  </fieldset>
+	  <fieldset class="control-group">
+	          <label for="disabledInput" class="control-label">Role</label>
+	          <div class="controls">
+	            <input type="text" disabled="" placeholder="<?php echo isset($settings->username) ? $settings->role : ''; ?>" name="role" class="span3 disabled">
+	          </div>
+	  </fieldset>
+	</div>
+	  
+	  <div class="span6">
+		  <legend>Change Password</legend>
+		  <fieldset class="control-group<?php if(form_error('newpassword')) echo ' error'; ?>">
+		          <label for="newpassword" class="control-label">New Password</label>
+		          <div class="controls">
+			        <?php echo form_password($attr_input_pwd); ?>
+		        	<?php echo form_error('newpassword','<p class="help-block">','</p>'); ?>
+		          </div>
+		  </fieldset>
+		  <fieldset class="control-group<?php if(form_error('newpassword2')) echo ' error'; ?>">
+		          <label for="newpassword2" class="control-label">Confirm Password</label>
+		          <div class="controls">
+			        <?php echo form_password($attr_input_pwd2); ?>
+		        	<?php echo form_error('newpassword2','<p class="help-block">','</p>'); ?>
+		          </div>
+		  </fieldset>	  	  	  
+	  </div>
+  </div> 
+  <div class="row">
+	  <div class="span12">
+	  	  <legend>Confirm Changes</legend>
+		  <fieldset class="control-group<?php if( isset($confirmation_error) ) echo ' error'; ?>">
+		          <label for="newpassword" class="control-label">Your Password</label>
+		          <div class="controls">
+			        <?php echo form_password($attr_input_conf); ?>
+		        	<span class="help-inline">Enter your current password to confirm changes.</span>
+		          </div>
+		  </fieldset>	  	
+	  </div> 
   </div>
-  <div class="span5 ">
-    <div class="alert-message block-message warning">
-    <strong>Notice:</strong>
-    Right now your are only allowed to change your email address and your password. Every other setting is fixed by the time you registered or can only be changed by an admin.
-    </div>
-  </div>
+  <div class="row">
+	  <div class="span12">  
+		<fieldset class="form-actions">
+	      <button class="btn large primary" type="submit">Save changes</button>
+	      <button class="btn large" type="reset">Cancel</button>
+		</fieldset>
+	  </div> 
+  </div>	   	  		   
+	<?php echo form_close(); ?>
+  
 </div>
