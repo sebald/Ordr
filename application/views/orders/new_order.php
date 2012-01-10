75a67a381eae6edbb1b73ef07189ef68d80864ae
Sebald <sebastian.sebald@gmail.com>
2012-01-10 17:46:15 +0100

    fixed a typo

--- application/views/orders/new_order.php
+++ application/views/orders/new_order.php
@@ -30,7 +30,7 @@
 		<div class="row">
 			<div class="span8">
 				<div class="alert-message block-message info">
-			        <p><strong>Notice!</strong> If you want to order an common consumable use the <em>search on the right</em>. Maybe the consumable is already in the data base. If so, the order form will be filled out for you.</p>
+			        <p><strong>Notice!</strong> If you want to order a common consumable use the <em>search on the right</em>. Maybe the consumable is already in the data base. If so, the order form will be filled out for you.</p>
 			    </div>
 				<form class="horizontal-form">
 					<legend>Item Information</legend>
