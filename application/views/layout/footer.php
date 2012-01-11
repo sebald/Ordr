a354a40c9080979fb787331b973fc28d77e5ec2d
Sebald <sebastian.sebald@gmail.com>
2012-01-11 11:06:32 +0100

    better user options dropdown instead of modal

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -2,26 +2,12 @@
   <footer>
     <p>Created by <a href="http://www.distractedbysquirrels.com">Sebastian Sebald</a></p>
   </footer>
-<script src="<?php echo base_url();?>js/jquery.js"></script>
-<script src="<?php echo base_url();?>js/bootstrap-transition.js"></script>
-<script src="<?php echo base_url();?>js/bootstrap-dropdown.js"></script>
-<script src="<?php echo base_url();?>js/bootstrap-modal.js"></script>
-<script src="<?php echo base_url();?>js/bootstrap-alerts.js"></script>
-<script src="<?php echo base_url();?>js/bootstrap-twipsy.js"></script>
-<script src="<?php echo base_url();?>js/functions.js"></script>
-<div class="modal hide fade" id="modal-user">
-            <div class="modal-header">
-              <a data-dismiss="modal" class="close" href="#">×</a>
-              <h3>Account</h3>
-            </div>
-            <div class="modal-body">
-				<ul class="unstyled">
-			    	<li class="option"><?php echo anchor('account/settings', 'Settings'); ?> - Change your account information and settings.</li>
-			  	</ul>
-            </div>
-            <div class="modal-footer">
-            	<?php echo anchor('account/logout', 'Log out','class="btn large danger"'); ?>
-            </div>
-</div>
+	<script src="<?php echo base_url();?>js/jquery.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap-transition.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap-dropdown.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap-modal.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap-alert.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap-twipsy.js"></script>
+	<script src="<?php echo base_url();?>js/functions.js"></script>
 <body>
 </html>
--- application/views/layout/topbar_logged_in.php
+++ application/views/layout/topbar_logged_in.php
@@ -1,1 +1,12 @@
-<p class="pull-right">Logged in as <a data-toggle="modal" href="#modal-user"><?php echo $this->session->userdata('username'); ?></a></p>
+<ul class="nav pull-right">
+	<li class="dropdown" id="user-options">
+		<a data-toggle="dropdown" class="dropdown-toggle" href="#">Logged in as <span class="user-name"><?php echo $this->session->userdata('username'); ?></span></a>
+		<ul class="dropdown-menu">
+		  <li><?php echo anchor('account/settings', 'Settings'); ?></li>
+		  <li><a href="#">Help</a></li>
+		  <li><a href="https://github.com/sebald/Ordr/issues">Submit an Issue</a></li>
+		  <li class="divider"></li>
+		  <li><?php echo anchor('account/logout', 'Log out'); ?></li>
+		</ul>
+	</li>               
+</ul>
