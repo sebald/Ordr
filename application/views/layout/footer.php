80ccf43b336a0748b8e8b519a20a74e0a8580f98
Sebald <sebastian.sebald@gmail.com>
2012-01-14 15:26:56 +0100

    added typeahead

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -8,6 +8,7 @@
 	<script src="<?php echo base_url();?>js/bootstrap-modal.js"></script>
 	<script src="<?php echo base_url();?>js/bootstrap-alert.js"></script>
 	<script src="<?php echo base_url();?>js/bootstrap-twipsy.js"></script>
+	<script src="<?php echo base_url();?>js/bootstrap-typeahead.js"></script>
 	<script src="<?php echo base_url();?>js/functions.js"></script>
 <body>
 </html>
