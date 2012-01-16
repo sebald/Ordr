2adc4ea4be3d04aa9a2cf99fffed2e689c74a7db
Sebald <sebastian.sebald@gmail.com>
2012-01-16 23:20:07 +0100

    added good old clear both to the footer

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,8 +1,10 @@
   </div>
   <footer>
     
-    <a class="icon-html" href="http://www.w3.org/html/" title="HTML5 Powered with CSS3 / Styling, and Semantics"></a>
-    <a class="icon-dbs" title="This software was written by Sebastian Sebald." target="_blank" href="http://distractedbysquirrels.com/"></a>
+    <div>
+    	<a class="icon-html" href="http://www.w3.org/html/" title="HTML5 Powered with CSS3 / Styling, and Semantics"></a>
+    	<a class="icon-dbs" title="This software was written by Sebastian Sebald." target="_blank" href="http://distractedbysquirrels.com/"></a>
+	</div>    	
     
   </footer>
 	<script src="<?php echo base_url();?>js/jquery.js"></script>
