95dc8c3e4f4fadb6c13318d13b8713d2e6d9c841
Sebald <sebastian.sebald@gmail.com>
2012-01-17 10:47:12 +0100

    footer and welcome screen adjustments

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -3,21 +3,21 @@
 		<div class="span12">
 			<hgroup id="welcome">
 				<h1>Welcome to <span class="brand">ordr</span>!</h1>
-				<p class="quote">An order management system to simplify your chemical shopping.</p>
+				<p class="quote">An order management system to simplify your shopping for laboratory supplies.</p>
 			</hgroup>
 		</div>
 	</div>
 	<div class="row">
 		<div class="span4">
-			<h2>Analog never again!</h2>
+			<h2>1. Register</h2>
 			<p>Aesthetic irony fanny pack synth, VHS thundercats PBR blog skateboard high life put a bird on it 3 wolf moon terry richardson trust fund. Tumblr bicycle rights ethical seitan messenger bag.</p>
 		</div>
 		<div class="span4">
-			<h2>Want a tour?</h2>
+			<h2>2. Place an Order</h2>
 			<p>Fixie pitchfork dreamcatcher mixtape. Helvetica sartorial before they sold out stumptown brunch fanny pack artisan, salvia food truck tumblr marfa squid. Before they sold out food truck wayfarers dreamcatcher.</p>
 		</div>
 		<div class="span4">
-			<h2>Be a part of it!</h2>
+			<h2>3. Get notified</h2>
 			<p>Before they sold out food truck wayfarers dreamcatcher. Yr fanny pack Austin portland skateboard, craft beer butcher high life mixtape terry richardson wolf farm-to-table viral brunch.</p>
 		</div>
 	</div>
