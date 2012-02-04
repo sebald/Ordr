2097bb5b6746bcf0ebe15fe4211034fb57e25ed7
Sebald <sebastian.sebald@gmail.com>
2012-02-04 13:18:40 +0100

    text for welcome page

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,4 +1,5 @@
 <div class="container">
+	
 	<div class="row">
 		<div class="span12">
 			<hgroup id="welcome">
@@ -7,18 +8,28 @@
 			</hgroup>
 		</div>
 	</div>
+	
+	<div class="row">
+		<div class="span12">
+			<p>
+				<strong>What can order do for you?</strong> It will simplify the process of ordering laboratory supplies by using the power of the newest web technologies. Interested? Just follow the three steps below!
+			</p>
+		</div>
+	</div>
+	
 	<div class="row">
 		<div class="span4">
 			<h2>1. Register</h2>
-			<p>Aesthetic irony fanny pack synth, VHS thundercats PBR blog skateboard high life put a bird on it 3 wolf moon terry richardson trust fund. Tumblr bicycle rights ethical seitan messenger bag.</p>
+			<p>Registration is easy as 1-2-3. Just fill out the form on <?php echo anchor('account/register', 'this page');?> and as soon as an admin has activated your account the shopping can begin!</p>
 		</div>
 		<div class="span4">
 			<h2>2. Place an Order</h2>
-			<p>Fixie pitchfork dreamcatcher mixtape. Helvetica sartorial before they sold out stumptown brunch fanny pack artisan, salvia food truck tumblr marfa squid. Before they sold out food truck wayfarers dreamcatcher.</p>
+			<p>A lot of the chemicals, supllies and so forth are already stored in the database, so you don't have to fill out the order form your self!</p>
 		</div>
 		<div class="span4">
 			<h2>3. Get notified</h2>
-			<p>Before they sold out food truck wayfarers dreamcatcher. Yr fanny pack Austin portland skateboard, craft beer butcher high life mixtape terry richardson wolf farm-to-table viral brunch.</p>
+			<p>As soons as your purchase has arrived you will automatically get notified. Or you can use the orders overview to check what the working status is.</p>
 		</div>
 	</div>
+	
 </div>
