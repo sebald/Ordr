fd0c91af011b85ed25ef907d6c68ac2437335866
Holger Frey <holgerfrey@remote190-130.home.uni-freiburg.de>
2012-04-13 10:32:25 +0200

    removed a surplus if statement
    If no users with the status "new" are present, there is a notice issued
    in the view that $new_users is not initialized.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,11 +1,11 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
  * Admin Controller.
  *
  * @package		Ordr
  * @subpackage	Controller
- * @category	
+ * @category
  * @author		Sebastian Sebald
  */
 class Admin extends MY_Controller {
@@ -15,21 +15,19 @@ class Admin extends MY_Controller {
                     'username'    => 'Username',
                     'email'       => 'Email',
                     'role'        => 'Role'
-        );   
+        );
 		$filter['role'] = 'new';
         $filter['display'] = array('username', 'email', 'role');
-    
+
         // get users
         $this->load->model('user_model');
         $query = $this->user_model->search(5, 0, 'date_created', 'asc', $filter);
 
         $data['table_users'] = FALSE;
-		if ( $query['count'] > 0 ) {       
-	        $data['new_users'] = $query['count'];
-		}
-	
+	    $data['new_users'] = $query['count'];
+
         $data['main_content'] = 'admin/index';
-        $this->load->view('layout/template', $data);    
+        $this->load->view('layout/template', $data);
     }
 
 
@@ -42,7 +40,7 @@ class Admin extends MY_Controller {
 	 * 	The redirect is needed because there is only one form action, but there are more than
 	 * 	one actions for table data.
 	 */
-	public function users_actions(){		
+	public function users_actions(){
 		// request search
 		if ( $this->input->post('action') == 'search' ) {
 			$this->session->set_flashdata('search', $this->input->post('search'));
@@ -51,7 +49,7 @@ class Admin extends MY_Controller {
 		// nothing selected for action => deny request
 		if ( $this->input->post('marked') == FALSE ){
 	        $msg = create_alert_message('warning', 'No can do!', 'Please select some records and try again.');
-			$this->session->set_flashdata('message', $msg);			
+			$this->session->set_flashdata('message', $msg);
 			redirect($_SERVER['HTTP_REFERER']);
 		}
 		// request based on action parameter
@@ -62,7 +60,7 @@ class Admin extends MY_Controller {
 				break;
 			case 'delete':
 				redirect('admin/users/delete');
-				break;				
+				break;
 			default:
 				redirect('admin/users/view');
 				break;
@@ -77,7 +75,7 @@ class Admin extends MY_Controller {
 	 * 	@param	order	asc or desc ordering
 	 * 	@param	page	used by the CI pagination
 	 */
-    public function users_view($query = 'all', $by = 'username', $order = 'asc', $page = 1) {   
+    public function users_view($query = 'all', $by = 'username', $order = 'asc', $page = 1) {
         $limit = 15;
         $data['fields'] = array(
                     'username' 		=> 'Username',
@@ -89,8 +87,8 @@ class Admin extends MY_Controller {
         $data['by'] = $by;
         $data['order'] = $order;
 		$data['query'] = $query;
-		
-		// parse query      
+
+		// parse query
 		$filter = FALSE;
 		if ( $query != 'all' ) {
 			parse_str($query, $filter);
@@ -121,16 +119,16 @@ class Admin extends MY_Controller {
         $config['per_page'] = $limit;
         $config['uri_segment'] = 7;
         $config['num_links'] = 5;
-        
+
         // pagination
         $this->load->library('pagination');
         $this->pagination->initialize($config);
-        $data['pagination'] = $this->pagination->create_links();        
-		
+        $data['pagination'] = $this->pagination->create_links();
+
         $data['main_content'] = 'admin/users_view';
         $this->load->view('layout/template', $data);
     }
-    
+
 	/**
 	 * 	Edit a single user.
 	 * 	@param	username	username of the user, which should be edited
@@ -153,27 +151,27 @@ class Admin extends MY_Controller {
 		// validate form
 	    if( $this->form_validation->run() == FALSE ) {
 	        // form error
-	    } else {      
+	    } else {
 	        // update settings
 	        $this->user_model->update($username);
 	        $msg = create_alert_message('success', 'Updated completed.', 'The account information has been updated successfully.');
 			$this->session->set_flashdata('message', $msg);
 			redirect('admin/users/edit/'.$username);
 	    }
-		
+
 		$data['user_categories'] = get_user_categories();
-		
+
 		$data['main_content'] = 'admin/users_edit';
         $this->load->view('layout/template', $data);
 	}
-	
+
 	/**
 	 * 	Change the role of users. This is one of the table actions.
 	 */
 	public function users_role() {
 		$this->load->model('user_model');
 		$users = $this->session->flashdata('marked');
-		
+
 		// role change confirmed?
 		if( $this->input->post('submit-role') ) {
 			foreach ($_POST['role'] as $user => $role) {
@@ -183,7 +181,7 @@ class Admin extends MY_Controller {
 			$msg = create_alert_message('success', 'Role update successfull!', count($_POST['role']).' users updated.');
 			$this->session->set_flashdata('message', $msg);
 			redirect('admin/users/view');
-		}		
+		}
 
 		// get users (by username)
 		$data['users'] = $this->user_model->get($users)->result();
@@ -194,13 +192,13 @@ class Admin extends MY_Controller {
                     'email'       => 'Email',
                     'role'        => 'Role'
         );
-		
+
 		$data['user_categories'] = get_user_categories();
-		
+
 		$data['main_content'] = 'admin/users_role';
         $this->load->view('layout/template', $data);
 	}
-	
+
 	/**
 	 * 	Delete user(s). This is one of the table actions, but can also be used for "quick deleting" a single user.
 	 */
@@ -210,25 +208,25 @@ class Admin extends MY_Controller {
 			$users = $this->session->flashdata('marked');
 		}
 		$this->session->keep_flashdata('marked');
-		
+
 		// deletion confirmed?
 		if( $this->input->post('submit-delete') ) {
 			$this->delete($users, 'User(s)');
 			die();
 		}
-				
+
 		$this->load->model('user_model');
 		$query = $this->user_model->get($users);
-		
+
 		$this->load->library('table');
 		$this->table->set_heading(array('Username', 'First Name', 'Last Name', 'Email', 'Role'));
 		$this->table->set_template(array ( 'table_open'  => '<table class="table">' ));
-        $data['table_users'] = $this->table->generate($query); 
-		
+        $data['table_users'] = $this->table->generate($query);
+
 		$data['main_content'] = 'admin/users_delete';
         $this->load->view('layout/template', $data);
 	}
-	
+
 	/**
 	 * 	Helper function for deleting user(s).
 	 */
@@ -238,16 +236,16 @@ class Admin extends MY_Controller {
 
 		$msg = create_alert_message('success', 'Deletion successfull!', count($data).' '.$type.' permantly deleted.');
 		$this->session->set_flashdata('message', $msg);
-		redirect('admin/users_view');	
+		redirect('admin/users_view');
 	}
-	
+
 	public function users_filter(){
 		$filter = 'all';
 		if( isset($_POST['filter']) )
 			$filter = http_build_query( array($_POST['filter']['by'] => $_POST['filter']['term']) );
 		redirect('admin/users/view/'.$filter);
 	}
-	
+
 	public function users_search(){
 		$search= 'all';
 		if( $this->session->flashdata('search') )
@@ -255,7 +253,7 @@ class Admin extends MY_Controller {
 		if( isset($_POST['display']) && $search != 'all' )
 			$search = 'display='.$_POST['display'].'&'.$search;
 		if( isset($_POST['display']) && $search == 'all' )
-			$search = 'display='.$_POST['display'];		
+			$search = 'display='.$_POST['display'];
 		redirect('admin/users/view/'.$search);
 	}
 
@@ -266,14 +264,14 @@ class Admin extends MY_Controller {
 		if( isset($_POST['role']) )
 			$display = $display.'&role='.$_POST['role'];
 		if( isset($_POST['search']) )
-			$display = $display.'&search='.str_replace(' ', '+', $_POST['search']);	
+			$display = $display.'&search='.str_replace(' ', '+', $_POST['search']);
 		redirect('admin/users/view/'.$display);
 	}
 
 	/********************************************************/
 	/*			  	    ADMIN - CONSUMABLES					*/
 	/********************************************************/
-	
+
 		/**
 	 * 	Displaying common consumables in a table and provide actions to manage them. All parameters are
 	 * 	optional.
@@ -301,9 +299,9 @@ class Admin extends MY_Controller {
 		$filter = FALSE;
 		if ( $query != 'all' ) {
 			parse_str($query, $filter);
-			$data['filter'] = $filter;	
+			$data['filter'] = $filter;
 		}
-			
+
         // get users
         $offset = ($page-1)*$limit;
         $this->load->model('consumables_model');
@@ -318,18 +316,18 @@ class Admin extends MY_Controller {
         $config['per_page'] = $limit;
         $config['uri_segment'] = 7;
         $config['num_links'] = 5;
-        
+
         // pagination
         $this->load->library('pagination');
         $this->pagination->initialize($config);
-        $data['pagination'] = $this->pagination->create_links(); 		
-		
+        $data['pagination'] = $this->pagination->create_links();
+
 		$data['consumable_categories'] = get_consumable_categories();
-		
+
 		$data['main_content'] = 'admin/consumables_view';
         $this->load->view('layout/template', $data);
 	}
-	
+
 	public function consumables_new(){
 		// field name, error message, validation rules
 	    $this->load->library('form_validation');
@@ -347,7 +345,7 @@ class Admin extends MY_Controller {
 
 	    if( $this->form_validation->run() == FALSE ) {
 	        $data['main_content'] = 'admin/consumables_new';
-	    } else {		
+	    } else {
 	        $this->load->model('consumables_model');
 	        if($this->consumables_model->create()) {
 	        	$msg = create_alert_message('success', 'Consumables added successfully!!', '<em>'.$this->input->post('CAS_description').'</em> added to the databse.');
@@ -356,12 +354,12 @@ class Admin extends MY_Controller {
 	    	} else {
 	        	$msg = create_alert_message('error', 'Something went wrong!!', 'The consumables has not been added to the databse.');
 				$this->session->set_flashdata('message', $msg);
-	        	$data['main_content'] = 'admin/consumables_new';			
+	        	$data['main_content'] = 'admin/consumables_new';
 	        }
-	    }		
+	    }
         $this->load->view('layout/template', $data);
 	}
-	
+
 	public function consumables_edit($id = FALSE) {
 		if ( $id ) {
 			$this->load->model('consumables_model');
@@ -369,7 +367,7 @@ class Admin extends MY_Controller {
 		} else {
 			$data['consumable'] = (object) $this->input->post();
 		}
-		
+
 		// field name, error message, validation rules
 	    $this->load->library('form_validation');
 	    $this->form_validation->set_rules('category', 'Category', 'trim|required');
@@ -377,11 +375,11 @@ class Admin extends MY_Controller {
 		$this->form_validation->set_rules('vendor', 'Vendor', 'trim|required');
 		$this->form_validation->set_rules('package_size', 'Package Size', 'trim|required');
 		$this->form_validation->set_rules('price_unit', 'Unit Price', 'trim|required');
-		$this->form_validation->set_rules('comment', 'Comment', 'trim|max_length[140]');		
-		
+		$this->form_validation->set_rules('comment', 'Comment', 'trim|max_length[140]');
+
 	    if( $this->form_validation->run() == FALSE ) {
 	        $data['main_content'] = 'admin/consumables_edit';
-	    } else {		
+	    } else {
 	        $this->load->model('consumables_model');
 	        if($this->consumables_model->update()) {
 	        	$msg = create_alert_message('success', 'Consumables updated successfully!!', 'The consumable <em>'.$this->input->post('CAS_description').'</em> has been updated.');
@@ -390,39 +388,39 @@ class Admin extends MY_Controller {
 	    	} else {
 	        	$msg = create_alert_message('error', 'Something went wrong!!', 'The consumables could not be updated.');
 				$this->session->set_flashdata('message', $msg);
-	        	redirect('admin/consumables/edit/'.$this->input->post('id'));			
+	        	redirect('admin/consumables/edit/'.$this->input->post('id'));
 	        }
 	    }
         $this->load->view('layout/template', $data);
 	}
-	
+
 	public function consumables_delete($id) {
 		if( $this->input->post('submit-delete') == 'confirm') {
 			$this->load->model('consumables_model');
 			if ( $this->consumables_model->delete($id) ) {
 				$msg = create_alert_message('success', 'Consumable deleted successfull!!', 'The consumable <em>'.$this->input->post('CAS_description').'</em> has been deleted.');
-				$this->session->set_flashdata('message', $msg);			
+				$this->session->set_flashdata('message', $msg);
 			} else {
 				$msg = create_alert_message('error', 'Something went wrong!!', 'The consumable <em>'.$this->input->post('CAS_description').'</em> could not be deleted.');
 				$this->session->set_flashdata('message', $msg);
 			}
 			redirect('admin/consumables/view');
 		}
-		
+
 		$this->load->model('consumables_model');
 		$query = $this->consumables_model->get($id, 'id', 'id, vendor, catalog_number, CAS_description, category, package_size, price_unit, currency');
 		$data['CAS_description'] = $query->row(0)->CAS_description;
-		
+
 		$this->load->library('table');
-		$this->table->set_heading(array('id', 'vendor', 'catalog_number', 'CAS_description', 'category', 
+		$this->table->set_heading(array('id', 'vendor', 'catalog_number', 'CAS_description', 'category',
 							'package_size', 'price_unit', 'currency'));
-		$this->table->set_template(array ( 'table_open'  => '<table class="table">' ));							
+		$this->table->set_template(array ( 'table_open'  => '<table class="table">' ));
         $data['table'] = $this->table->generate($query);
-		
+
 		$data['main_content'] = 'admin/consumables_delete';
 		$this->load->view('layout/template', $data);
 	}
-	
+
 }
 
 /* End of file admin.php */
