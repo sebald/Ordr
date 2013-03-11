ebb919d2db1baabaebd43a5dc762a606838ade4c
Holger Frey <holgerfrey@fp-192-52-24-203.mobile.uni-freiburg.de>
2013-03-11 15:35:41 +0100

    Changed "Equipment" to "Disposable"
    This was an request from Ossi

--- application/helpers/taxonomies_helper.php
+++ application/helpers/taxonomies_helper.php
@@ -8,7 +8,7 @@ if (!function_exists('get_currencies')) {
 
 if (!function_exists('get_consumable_categories')) {
 	function get_consumable_categories() {
-		return array( 'Chemical', 'Equipment', 'Solvent', 'BioLab' );
+		return array( 'Chemical', 'Disposable', 'Solvent', 'BioLab' );
 	}
 }
 
